4932b940984c6fa76884d8751e4051e3c3af0ddc
iriasgithub <iria.pardo.neira@udc.es>
2024-02-09 10:54:30 +0100

    [EJ-1] Implementación de la función mapdoble y respuestas a las preguntas.

--- P0.ml
+++ P0.ml
@@ -1,1 +1,20 @@
-mapdoble (function x -> x) (function x -> -x) [1;1;1;1;1];;
+(*Implementación de la función mapdoble*)
+let rec mapdoble f1 f2 l = match l with
+  [] -> []
+  | h1::h2::t -> f1 h1::f2 h2::mapdoble f1 f2 t
+  | h1::[] -> f1 h1::[];;
+
+(*Tipo de la función mapdoble*)
+(*val mapdoble : ('a -> 'b) -> ('a -> 'b) -> 'a list -> 'b list = <fun>*)
+
+(*Valor de: 
+mapdoble (function x -> x*2) (function x -> "x") [1;2;3;4;5];;
+Error: This expression has type string but an expression was expected of type int
+mapdoble espera que ambas funciones devuelvan el mismo tipo de dato, que ha de ser el tipo 
+de dato de toda la lista*)
+
+(*Tipo de *)
+let y = function x -> 5 in mapdoble y;;
+(*- : ('_weak1 -> int) -> '_weak1 list -> int list = <fun>
+Estamos indicando que el primer argumento que recibe mapdoble es la función x -> 5
+por lo que el tipo de "mapdoble y" es una función parcialmente aplicada. De ahí los weak*)
